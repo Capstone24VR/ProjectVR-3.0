d56a57b3374a8338a1eace9e604ab57cb8378e02
skd225 <skd225@lehigh.edu>
2024-10-22 16:33:48 -0400

    err idk

--- Assets/Our Minigames/Scripts/Cards/Card.cs
+++ Assets/Our Minigames/Scripts/Cards/Card.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using XRMultiplayer.MiniGames;
 
-public class Card : MonoBehaviour
+public class Card : NetworkBehaviour
 {
     public enum Suit { Heart, Diamond, Club, Spades }
     public enum Value
@@ -33,11 +37,17 @@ public class Card : MonoBehaviour
     public bool played = false;
 
     [SerializeField] protected Vector3 _position = Vector3.zero;
-    [SerializeField] protected Vector3 _localScale = Vector3.zero;
+    [SerializeField] protected Vector3 _localScale = Vector3.one;
+
+    private XRGrabInteractable _xrInteract;
+    private NetworkedCards _cardManager;
 
     public void Awake()
     {
         _localScale = transform.localScale;
+        _xrInteract = GetComponent<XRGrabInteractable>();
+
+        _cardManager = FindAnyObjectByType<NetworkedCards>();
     }
 
     public void SetPosition(Vector3 position)
@@ -53,14 +63,35 @@ public class Card : MonoBehaviour
         transform.localRotation = Quaternion.identity;
     }
 
+    // Hover select effect
     public void HoverSelect()
     {
-        transform.localScale = _localScale * 1.25f;
+        ScaleCard(_localScale * 1.25f);
     }
 
+    // Hover deselect effect
     public void HoverDeSelect()
     {
-        transform.localScale = _localScale;
+        ScaleCard(_localScale);
+    }
+
+    private void ScaleCard(Vector3 newScale)
+    {
+        // Update local scale
+        transform.localScale = newScale;
+
+        // If the server is executing this, synchronize the scale to all clients
+        if (IsServer)
+        {
+            RpcScaleCard(newScale);
+        }
+    }
+
+
+    private void RpcScaleCard(Vector3 scale)
+    {
+        // Apply the scale change on all clients
+        transform.localScale = scale;
     }
 
     public void SetInHand(bool isInHand)
@@ -68,16 +99,45 @@ public class Card : MonoBehaviour
         inHand = isInHand;
         if (inHand)
         {
-            //Debug.Log($"Card {suit} {value} is now in hand.");
+            // Debug.Log($"Card {suit} {value} is now in hand.");
         }
         else
-        { 
-            //Debug.Log($"Card {suit} {value} is no longer in hand.");
+        {
+            // Debug.Log($"Card {suit} {value} is no longer in hand.");
         }
     }
 
     public string GetCardId()
     {
         return suit.ToString() + value.ToString();
+    }
+
+    // Use XR Interaction Toolkit's hover callbacks to trigger hover effects
+    protected virtual void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        HoverSelect(); // Trigger hover select effect
+    }
+
+    protected virtual void OnHoverExited(HoverExitEventArgs args)
+    {
+        HoverDeSelect(); // Trigger hover deselect effect
+    }
+
+    private void OnEnable()
+    {
+        if (_xrInteract != null)
+        {
+            _xrInteract.hoverEntered.AddListener(OnHoverEntered);
+            _xrInteract.hoverExited.AddListener(OnHoverExited);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_xrInteract != null)
+        {
+            _xrInteract.hoverEntered.RemoveListener(OnHoverEntered);
+            _xrInteract.hoverExited.RemoveListener(OnHoverExited);
+        }
     }
 }
