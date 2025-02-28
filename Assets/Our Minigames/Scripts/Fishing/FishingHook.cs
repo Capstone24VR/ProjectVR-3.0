2091ecc6b9d050d12662cd024a52d442c12eaf2b
Selase-Dzathor <111992571+Selase-Dzathor@users.noreply.github.com>
2025-02-28 16:17:16 -0500

    everything at the kitchensink

--- Assets/Our Minigames/Scripts/Fishing/FishingHook.cs
+++ Assets/Our Minigames/Scripts/Fishing/FishingHook.cs
@@ -42,12 +42,14 @@ public class FishingHook : NetworkBehaviour
                 // Notify clients about the catch
                 NotifyCatchClientRpc(fishNetworkId);
 
+                // Update fish state
+                fishNetworkObject.GetComponent<NetworkedFishAI>().SetFishStateServerRpc(NetworkedFishAI.FishState.Struggle);
+
                 Debug.Log($"Fish got caught, Setting owner to Client: {GetComponent<NetworkObject>().OwnerClientId}");
                 if(fishNetworkObject.OwnerClientId != GetComponent<NetworkObject>().OwnerClientId)
                     fishNetworkObject.ChangeOwnership(GetComponent<NetworkObject>().OwnerClientId);
 
-                // Update fish state
-                fishNetworkObject.GetComponent<NetworkedFishAI>().SetFishStateServerRpc(NetworkedFishAI.FishState.Struggle);
+
             }
             else
             {
